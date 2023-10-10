e4a35ca325b2bb4d58107714a6111ad8e64b6094
Lukas Chou <lukaschou21@icloud.com>
2023-10-09 19:04:07 -0700

    put text in middle

--- TextActions.c
+++ TextActions.c
@@ -1,7 +1,10 @@
 #include <ncurses.h>
 #include <unistd.h>
+#include "TextActions.h"
+
 
 void type_text(char* text) {
+    move(5,10);
     for (int i = 0; text[i] != '\0'; i++) {
         addch(text[i]); 
         refresh(); 
