28330c77286d8057cf62dbceefdb59874bbee6e9
calebchou21 <calebchou21@gmail.com>
2023-10-09 19:19:43 -0600

    HEADER

--- main.c
+++ main.c
@@ -6,16 +6,16 @@ int main() {
     noecho(); // Disable echoing of typed characters
     curs_set(FALSE); // Hide the cursor
 
-    char* text = "My name is Jeff lmaooooooooooooooo";
-    
-    for (int i = 0; text[i] != '\0'; i++) {
-        addch(text[i]); // Print the character
-        refresh(); // Refresh the screen
-        napms(30); // Delay for 100 milliseconds (adjust as needed)
-    }
+   char* text = "My name is Jeff lmaooooooooooooooo";
 
-    getch(); // Wait for user input
-    endwin(); // Cleanup ncurses
-    return 0;
+   for (int i = 0; text[i] != '\0'; i++) {
+      addch(text[i]); // Print the character
+      refresh(); // Refresh the screen
+      napms(30); // Delay for 100 milliseconds (adjust as needed)
+   } 
+   
+   getch(); // Wait for user input
+   endwin(); // Cleanup ncurses
+   return 0;
 }
 
---
+++ text_actions.c
@@ -0,0 +1,14 @@
+#include "text_actions.h"
+
+/* void type_text(*char text) {
+    for (int i = 0; text[i] != '\0'; i++) {
+        addch(text[i]); 
+        refresh(); 
+        napms(30); 
+   } 
+} */
+
+/* int main(){
+    char* text1 = ["This is a test case"];
+    type_text(text1);
+} */
