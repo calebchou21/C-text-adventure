236a5be756dc3b41650736bff03de8bbdcc747bf
Lukas Chou <lukaschou21@icloud.com>
2023-10-09 18:33:23 -0700

    moved code out of main

--- main.c
+++ main.c
@@ -1,21 +1,6 @@
-#include <ncurses.h>
 #include <unistd.h>
 
 int main() {
-    initscr(); // Initialize ncurses
-    noecho(); // Disable echoing of typed characters
-    curs_set(FALSE); // Hide the cursor
-
-   char* text = "My name is Jeff lmaooooooooooooooo";
-
-   for (int i = 0; text[i] != '\0'; i++) {
-      addch(text[i]); // Print the character
-      refresh(); // Refresh the screen
-      napms(30); // Delay for 100 milliseconds (adjust as needed)
-   } 
-   
-   getch(); // Wait for user input
-   endwin(); // Cleanup ncurses
    return 0;
 }
 
