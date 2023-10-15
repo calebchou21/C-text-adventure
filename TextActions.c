75c839d48a7fa36abb583cdf9c4c4848f0a2a07f
Lukas Chou <lukaschou21@icloud.com>
2023-10-15 13:23:40 -0700

    type text function code refactor

--- TextActions.c
+++ TextActions.c
@@ -15,22 +15,36 @@ void screen_cleanup(void) {
 
 void type_text(char* text) {
     int max_y, max_x;
-    int cols = 25;
     getmaxyx(stdscr, max_y, max_x); 
-    move(5, 25);
+    int left_margin = max_x / 9;
+    int right_margin = max_x - left_margin - 5;
+    int cols = left_margin;
+
+    move(5, left_margin);
+
     for (int i = 0; text[i] != '\0'; i++) {
         if (cols == 25 && text[i] == ' ') {continue;}
-        if (cols >= max_x - 30) {
-            if (text[i - 1] != ' ')
+        if (cols >= right_margin && cols <= (right_margin + 5)) {
+            if (text[i] == ' ') {
+                while(text[i] == ' '){
+                    i++;
+                }
+                addch('\n');
+                move(getcury(stdscr) + 1, left_margin);
+                cols = left_margin;
+            }
+        } else if (cols > (right_margin + 5)){
+            if (text[i] != ' '){
                 addch('-');
+            }
             addch('\n');
-            move(getcury(stdscr) + 1, 25);
-            cols = 25;
+            move(getcury(stdscr) + 1, left_margin);
+            cols = left_margin;
         }
-            addch(text[i]);
-            cols++;
-            refresh(); 
-            napms(15);
+        addch(text[i]);
+        cols++;
+        refresh(); 
+        napms(15);
     }
     getch();
     clear();
