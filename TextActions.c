0ba7822b133d0b96c7cb80b824b2d9c22952f063
calebchou21 <calebchou21@gmail.com>
2023-10-15 14:21:12 -0600

    type and select options

--- TextActions.c
+++ TextActions.c
@@ -37,6 +37,81 @@ void type_text(char* text) {
     refresh();
 }
 
-void type_options(char* options[]){
+void type_options(char** options, int num_options){
+    int max_y, max_x;
+    int rows = 5;
+    getmaxyx(stdscr, max_y, max_x);
+    max_y = 0; //Temporary, gets rid of warning
+    
+    for (int i = 0; i < num_options; i++) {
+        move(rows, 25);
+        add_option_nums(i, true);
 
+        for (int j = 0; options[i][j] != '\0'; j++) {
+            addch(options[i][j]);
+            refresh(); 
+            napms(30);
+        }
+        napms(350);
+        rows += 1;
+    }
+    
+    get_selected_option(options, num_options);
+}
+
+int get_selected_option(char** options, int num_options){
+    int selected = 0;
+    keypad(stdscr, TRUE);
+    while(1){
+        clear();
+        int rows = 5;
+        for (int i = 0; i < num_options; i++) {
+            if (i == selected) {
+                attron(A_REVERSE); 
+            }
+            move(rows, 25);
+            add_option_nums(i, false);
+            addstr(options[i]);
+            if (i == selected) {
+                attroff(A_REVERSE);
+            }
+            rows++;
+        }
+
+        int ch = getch();
+        switch (ch) {
+            case KEY_UP:
+                selected = (selected - 1 + num_options) % num_options;
+                break;
+            case KEY_DOWN:
+                selected = (selected + 1) % num_options;
+                break;
+            case 10: // Enter key
+                keypad(stdscr, FALSE);
+                return selected + 1;     
+        }
+        refresh();
+    }
+    return 0;
+    
+}
+
+void add_option_nums(int i, bool delay){
+    if (delay){
+        addch(i + 1 + '0');
+        refresh(); 
+        napms(50);
+        addch(':');
+        refresh(); 
+        napms(50);
+        addch(' ');
+        refresh(); 
+        napms(50);
+    } 
+    else {
+        addch(i + 1 + '0');
+        addch(':');
+        addch(' ');
+    }
+    
 }
--- TextActions.h
+++ TextActions.h
@@ -5,8 +5,11 @@
 #include <unistd.h>
 
 void type_text(char* text);
-void type_options(char* options[]);
+void type_options(char** options, int num_options);
+void add_option_nums(int i, bool delay);
 void screen_setup(void);
 void screen_cleanup(void);
+int get_selected_option(char** options, int num_options);
+
 
 #endif  
